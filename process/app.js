4fadbcfed9f26c207441286acbdc465bdeca019e
Ben Brunton <benbrunton@hotmail.com>
2015-01-27 21:47:56 +0000

    Started on reddit stuff and amended gitignore

--- process/app.js
+++ process/app.js
@@ -2,9 +2,15 @@
 
 var Q       = require('q');
 
+var userAgent = 'slabs.io.example.myredditapp:v1.0.0 (by /u/rust-dev)';
+
+var clientId = 'T3Y9ALPBq-oryQ';
+var client_secret = process.env.reddit_secret;
+
 exports.getData = function(settings) {
     var deferred = Q.defer();
     
+    var access_token = 'https://www.reddit.com/api/v1/access_token'
 
     return deferred.promise;
 };
